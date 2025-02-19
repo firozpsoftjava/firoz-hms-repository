fa582ac6ba732410999f6b1af273fc3a9dc77a89
firoz <firoz.powerbi@gmail.com>
2025-02-19 11:04:38 +0530

    Testing req

---
+++ src/main/java/com/hmsspring/healthcaremanagement/HealthcaremanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hmsspring.healthcaremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcaremanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcaremanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hmsspring/healthcaremanagement/HealthcaremanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmsspring.healthcaremanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcaremanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
